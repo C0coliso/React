b3a6c8ee7471026d61cd3c7815f2b4d28ec4b281
Christian <Christianvalleoliveira@gmail.com>
2025-10-14 19:13:42 +0200

    Primer commit React

---
+++ my-app/src/components/header/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './header.css';
+
+class Header extends React.Component{
+    render(){
+        return(
+            <div id="header">
+                <h1>
+                    Bienvenido
+                </h1>
+            </div>
+        )
+    }
+}
+
+export default Header;
