3127155f92adc75b5d441e92a03e66fafe1a25e2
Christian <Christianvalleoliveira@gmail.com>
2025-10-22 14:12:48 +0200

    Cambio en la actividad 03 act1b

--- my-app/src/components/main/App.js
+++ my-app/src/components/main/App.js
@@ -1,29 +1,21 @@
-import React from 'react';
-import MiLista from '../../MiLista.js'
+import MiLista from '../MiLista.js'
 import './App.css';
 import Header from '../header/Header.js'
-import footer from '../footer/Footer.js'
 import Footer from '../footer/Footer.js';
 
 function App() {
   return (
-    <div>
+    <>
       <Header/>
       <h2>Este es mi primer componente React</h2>
-      <div className='parrafo'>
+      <div id='parrafo'>
         <p>Bienvenido</p>
       </div>
 
-      <br/>
-      <MiLista titulo="Clientes" nombre1="Santiago" nombre2="Mateo" nombre3="Micaela" nombre4="Fabian"/>
-      <br/>
-      <MiLista titulo="Amigos" nombre1="Santiago" nombre2="Mateo" nombre3="Micaela" nombre4="Fabian"/>
-      <br/>
-      <MiLista titulo="Familiares" nombre1="Santiago" nombre2="Mateo" nombre3="Micaela" nombre4="Fabian"/>
-    
+    <MiLista/>
     <Footer/>
-    </div>
-  );
+    </>
+  )
 }
 
 
